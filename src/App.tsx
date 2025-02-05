706478bf709caf83f452d789686c603560c5535c
xiangnuans <cocoguo7636@gmail.com>
2025-02-05 21:13:25 +0800

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import "./App.css";
 import EditorComponent from "./components/Editor";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <div>
       <EditorComponent width="100%" height="100%" defaultLanguage="en" />
